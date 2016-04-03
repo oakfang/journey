32724fd9de3f46535cba5afb31de0d6ac1ac7ccd
Alon Niv <oakfang@gmail.com>
2016-04-03 11:21:17 +0300

    v. bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='journey',
-      version='0.1.0',
+      version='0.1.1',
       description='A pluginable input processing system (python 3)',
       url='http://github.com/oakfang/journey',
       author='Alon Niv',
